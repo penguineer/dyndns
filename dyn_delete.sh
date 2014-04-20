66429084aae7ec3cece04f00b7117de72fdbe2ec
Stefan Haun <tux@netz39.de>
2014-04-21 00:49:46 +0200

    dyn_delte: delete dynamic zones

---
+++ dyn_delete.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+## Delete an Antiblau dyn domain.
+#
+# Author: Stefan Haun <tux@netz39.de>
+#
+# Usage: dyn_delete domain
+#
+#	domain	is the domain to be deleted
+
+
+# Local config
+#ZONEDIR=/etc/bind/dynamic # TODO change for production use
+ZONEDIR=$(pwd)
+
+
+# Includes (see http://stackoverflow.com/a/12694189)
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/dyn_util.sh"
+
+
+
+# Get the domain name
+DOMAIN=$1
+# Check
+ERR=$(check_domain "$DOMAIN")
+if [ "$?" != "0" ]; then
+  echo "Domain parameter error: $ERR"
+  exit 1
+fi
+
+
+# Render the zone file path
+ZONEFILE=$ZONEDIR/$DOMAIN.zone
+# delete if exists
+if [ -f "$ZONEFILE" ]; then
+  rm -f "$ZONEFILE"
+else
+  echo "Warn: Zone file for the domain does not exist!"
+fi
+
+# Render the key file path
+KEYFILE=$ZONEDIR/$DOMAIN.key
+# abort if the key already exists
+if [ -f "$KEYFILE" ]; then
+  rm -f "$KEYFILE"
+else
+  echo "Warn: Key file for the domain does not exist!"
+fi
+
+
+# Finished
+echo "Dynamic zone for $DOMAIN deleted."
+# named config in zone.conf.dynamic is created by a different script
+echo "Make sure that the named zone.conf.dynamic is updated, too!"
+
+exit 0
+
+# End
